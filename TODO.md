8f19ce298ae3ac857d08910f49672d58cc88e3f2
Kelthon <kelthonbalbino@gmail.com>
2023-05-15 14:15:02 -0300

    first commit

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO.md
+
+## TO DO
+
+## DOING
+
+## DONE
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+int main(int argc, char **argv) {
+
+    srand(time(NULL));
+
+    return 0;
+}
